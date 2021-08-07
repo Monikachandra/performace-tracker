c8d344986342388d1b42a82f6dc2d011b4e715af
Monika chandra <monikachandrayadav@gmail.com>
2021-08-07 23:36:56 +0530

    renamed main to register

---
+++ login.php
@@ -0,0 +1,26 @@
+form method="post" action=login.php>
+<div class="input">
+<label>Enter your Vardhaman email ID</label>
+<input type="text" name="vemail" pattern=".+@Vardhaman\.org" required>
+</div>
+
+<div class="input">
+<label>Password :</label>
+<input type="Password" name="Password">
+</div>
+<div class="input">
+<button type="submit" name="Register" class="btn">REGISTER</button>
+</div>
+<p> Not a member yet? <a href="register.php">LOG IN </a><p>
+
+
+
+</form>
+
+
+
+</center>
+
+ </body>
+  </html>
+ 
