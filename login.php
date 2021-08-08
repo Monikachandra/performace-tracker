d23e80b62b4d34c069e4bd37d73aa477de4cc9cd
geetha-reddy <geethareddy.maale19@gmail.com>
2021-08-08 19:02:40 +0530

    first

--- login.php
+++ login.php
@@ -7,7 +7,6 @@
  
  <form method="post" action=login.php>
 <div class="input">
-    <!-- thi si sthehjkhjk -->
 <label>Enter your Vardhaman email ID</label>
 <input type="text" name="vemail" pattern=".+@Vardhaman\.org" required>
 </div>
