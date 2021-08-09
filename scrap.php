8c213c1bd7b1a5ed5fa2be35cfa8cb3a1b339830
Monika chandra <monikachandrayadav@gmail.com>
2021-08-09 15:32:29 +0530

    dom

--- scrap.php
+++ scrap.php
@@ -1,12 +1,12 @@
 <?php
-$url="https://www.codechef.com/users/monikachandra";
-$html=file_get_contents($url);
-echo $html;
-# $dom= new domDocument;
-#@$dom->loadHTML($html);
-#$tables=$dom->
-
-
-
+include('simple_html_dom.php');
+$html=file_get_contents('https://www.codechef.com/users/monikachandra');
+#echo $html;
+#$dom= new domDocument;
+#@-->ignore the error
+//@$dom->loadHTML($html);
+//$chefscore=$dom->getElementsByClassName('tabs-content');
+#rating-data-section problems-solved'tabs-content')
+echo $html->find('rating-data-section problems',0)->plaintext
 
 ?>
