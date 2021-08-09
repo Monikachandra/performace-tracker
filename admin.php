1755d50e143a1725247eeb1837800ea68a7bff5d
Monika chandra <monikachandrayadav@gmail.com>
2021-08-09 10:07:37 +0530

    adminpage

---
+++ admin.php
@@ -0,0 +1,46 @@
+<html>
+ <head>
+ <link rel="stylesheet" type="text/css" href="style.css">
+ </head>
+ <body>
+ <div class="heading">
+             <h2>HEY!! I AM ADMIN</h2>
+            </div>
+<form method="post" action=>
+
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY NAME</button>
+</div>
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY PASSED OUT YEAR</button>
+</div>
+
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY HACKER RANK SCORE</button>
+</div>
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY CODECHEF SCORE</button>
+</div>
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY CODEFORCES SCORE</button>
+</div>
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY CODECHEF RATING</button>
+</div>
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY CODEFORCES RATING</button>
+</div>
+<div class="input">
+<button type="button" name="name" class="btn">SORT BY SPOJ SCORE</button>
+</div>
+
+
+</form>
+
+
+
+</center>
+
+ </body>
+  </html>
+ 
