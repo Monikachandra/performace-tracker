9909f89b002dfbcd23a33ace6031e4286cbfad95
Monika chandra <monikachandrayadav@gmail.com>
2021-08-09 23:28:22 +0530

    monikaaaaaaaaaaaaaa

--- details.php
+++ details.php
@@ -1,5 +1,4 @@
- 
-<html>
+ <html>
 <head>
 <style>
 table
@@ -64,48 +63,24 @@ echo "<tr>";
 echo "<td>PassOutYear</td>";
 echo "<td>" .$data['year']. "</td>";
 echo "</tr>";
-
 echo "</table>";
 ?>
+<form method="post" >
+<div class="input">
+<label>Enter your CODECHEF USERNAME</label>
+<input type="text" name="chef" >
+</div>
+</form>
 <br>
 <br>
 <button onclick="myFunction()">codechef</button><br><br>
 <script>
 function myFunction(){
 <?php
-$html = file_get_contents('https://www.codechef.com/users/geethamaale'); //get the html returned from the following url
-
-$pokemon_doc = new DOMDocument();
-
-libxml_use_internal_errors(TRUE); //disable libxml errors
-
-if(!empty($html)){ //if any html is actually returned
-
-	$pokemon_doc->loadHTML($html);
-	libxml_clear_errors(); //remove errors for yucky html
-	
-	$pokemon_xpath = new DOMXPath($pokemon_doc);
-
-	//get all the h2's with an id
-	$pokemon_row = $pokemon_xpath->query('//*[@id="cumulative"]/div[1]/div[1]/div/a/text()[1]');
-
-	if($pokemon_row->length >0){
-		foreach($pokemon_row as $row){
-			$x=$row->nodeValue . "<br/>";
-			$y=substr($x,0,4);
-
-			$x=(int)$y;
-			header("Location: details.php?cf=".$x );
-		}
-	}
-
-
-}
-
+$x=$_POST['chef'];
+header("Location: scrap.php?cf=".$x );
 ?>
-
 }
 </script>
 </body>
 </html>
-
